d8546ef96df8fecf1078eec33b8a5d3332dbef7c
Wallo <wallo@gmail.com>
2023-06-29 15:06:10 +0200

    upd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI,Depends,HTTPException,status
+from fastapi import FastAPI, Request, Form, Depends,HTTPException,status
 from pydantic import BaseModel
+from typing import List
+from fastapi.templating import Jinja2Templates
+from starlette.responses import JSONResponse
 import joblib
 import uvicorn
 
@@ -11,6 +14,10 @@ class StartupData(BaseModel):
     administration: float =121344
     marketingspend: float =211025
 
+
+# JINJA2
+templates = Jinja2Templates(directory="templates")
+
 ## blocco per la cache del mio modello
 @app.on_event("startup")
 def startup_event():
@@ -21,6 +28,26 @@ def startup_event():
     return model
 
 ##########################################################################################################
+################################# SERVER JINJA2 + INFERENCE ##############################################
+
+@app.get("/",status_code=200)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.post("/predict")
+async def predict(request: Request,rd: float = Form(...), #<input name ="sepal_len">
+                             admin: float = Form(...), #<input name ="sepal_wid">
+                             marksd: float = Form(...), #<input name ="petal_len">
+                            ):
+    #target_names = load_iris().target_names.tolist()
+    target_names = ['R&D Spend', 'Administration', 'Marketingspend']
+    data = StartupData(rdspend=rd, administration=admin, marketingspend=marksd)
+    X = [[data.rdspend, data.administration, data.marketingspend]]
+    y_pred = model.predict(X)
+    prediction = Prediction(target=y_pred[0], target_names=target_names)
+    return templates.TemplateResponse("home.html", {"request": request,"prediction": prediction})
+
+##########################################################################################################
 ################################# CHIAMATE DIRETTE GET POST ##############################################
 
 @app.get("/")
@@ -28,7 +55,7 @@ def home():
     return {" ---->          http://localhost:8000/docs     <----------"}
 
 ## secca GET per streamlit o chiamate esterne
-@app.get("/predict")
+@app.get("/predict2")
 async def predictget(data:StartupData=Depends()):
     try:
         X = [[data.rdspend, data.administration, data.marketingspend]]
